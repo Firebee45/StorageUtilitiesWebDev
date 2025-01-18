b87c634e2641cb1547ad6720e02f3837da1de88d
Firebee <160149106+Firebee45@users.noreply.github.com>
2025-01-18 01:25:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function command() {
+    document.getElementById("dd1").innerHTML = "● COMMANDS";
+    document.getElementById("dd2").innerHTML = "○ RULES!";
+    document.getElementById("output").innerHTML = "./utilityitems <font color='#d90000'>𝙘𝙧𝙚𝙖𝙩𝙞𝙫𝙚 𝙤𝙣𝙡𝙮</font> <br> <br> <font color='#a3a3a3'>Spawns a barrel of useful items at your feet. It includes things like pre-filled filters, SS barrels, furnaces and pots, command obtained blocks like structure blocks and barriers, etc etc </font><br> <br> ./ss [container] [value] <br> <br><font color='#a3a3a3'> Tells you the necessary steps to get a specified SS from a given container. Currently works for barrels, chests, hoppers, droppers, pots, furnaces, lecterns and composters.</font> <br> <br> ./itemset [name] <font color='#d90000'>𝙘𝙧𝙚𝙖𝙩𝙞𝙫𝙚 𝙤𝙣𝙡𝙮</font> <br><br><font color='#a3a3a3'> Loads an item set based off data we collected from many servers. Ideal for testing. You can see the full list of available sets in the ./help menu </font> <br> <br> ./itemsummon [item] [amount] [location] <font color='#d90000'>𝙘𝙧𝙚𝙖𝙩𝙞𝙫𝙚 𝙤𝙣𝙡𝙮</font> <br><br><font color='#a3a3a3'> Summons items at a given location from thin air. It has a script event for command block use. The command is: /scriptevent storageutilities:itemsummon [item] [amount] [location] </font> <br><br>./summon [entity] [amount] <font color='#d90000'>𝙘𝙧𝙚𝙖𝙩𝙞𝙫𝙚 𝙤𝙣𝙡𝙮</font><br><br><font color='#a3a3a3'>Summons a given amount of a specified entity at your location</font";
+
+
+}
+
+function rule() {
+    document.getElementById("dd1").innerHTML = "○ COMMANDS";
+    document.getElementById("dd2").innerHTML = "● RULES!";
+    document.getElementById("output").innerHTML = "./canopy StorageUtilities <br><br> <font color='#a3a3a3'>Needs to be true for Storage Utilities to function</font><br><br>./canopy creativeOnly <br><br> <font color='#a3a3a3'>Needs to be true for all specified creative only commands.</font>";
+}
+
+/*
+<font color='#a3a3a3'>
+
+*/
+
